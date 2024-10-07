b122d827240ed892b85584c8b39f14b1f446c859
henrique-fleith <henrique.v.fleith@gmail.com>
2024-10-07 12:46:01 -0300

    foca pulante

--- front-end-eptran/Template/Template.js
+++ front-end-eptran/Template/Template.js
@@ -4,19 +4,27 @@ var header = document.getElementById("header")
 var footer = document.getElementById("footer")
 var overlay = document.getElementById("overlay")
 var planet = document.getElementById("planet")
+var car = document.getElementById("car")
+
 
 function collapse(){
 	if (collapsed == 1){
 		sideBar.style.width = "0px";
 		sideBar.style.fontSize = "0px";
 		overlay.style.opacity = "0%";
-		//planet.style.left = "100vw";
+		planet.style.left = "100vw";
+		car.style.left = "100vw";
+		planet.style.opacity = "0%";
+		car.style.opacity = "0%";
 	} 
 	else if (collapsed == -1){
 		sideBar.style.width = "240px";
 		sideBar.style.fontSize = "16px";
 		overlay.style.opacity = "50%";
-		//planet.style.left = "77vw";
+		planet.style.left = "77vw";
+		car.style.left = "75vw";
+		planet.style.opacity = "100%";
+		car.style.opacity = "100%";
 	};
 
 
