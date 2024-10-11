924edd47298e2608bb796fda3287601801f7db04
Lucas Lohmann <lucasvlohmann@gmail.com>
2024-10-11 11:39:33 -0300

    foca mudade teste 1

--- front-end-eptran/Template/Template.js
+++ front-end-eptran/Template/Template.js
@@ -3,26 +3,31 @@ var sideBar = document.getElementById("sideBar")
 var header = document.getElementById("header")
 var footer = document.getElementById("footer")
 var overlay = document.getElementById("overlay")
-var planet = document.getElementById("planet")
-var car = document.getElementById("car")
-
+var left = document.getElementById("left")
+var right = document.getElementById("right")
 
 function collapse(){
 	if (collapsed == 1){
-		sideBar.style.width = "0px";
+		sideBar.style.width = "0vh";
 		sideBar.style.fontSize = "0px";
 		overlay.style.opacity = "0%";
 		overlay.style.visibility = "hidden";
-		foca.style.left = "200vw";
-		foca.style.opacity = "0%";
+		
+		left.style.opacity = "0%";
+		left.style.visibility = "hidden";
+		right.style.opacity = "0%";
+		right.style.visibility = "hidden";
 	} 
 	else if (collapsed == -1){
-		sideBar.style.width = "240px";
+		sideBar.style.width = "30vh";
 		sideBar.style.fontSize = "16px";
 		overlay.style.opacity = "50%";
 		overlay.style.visibility = "visible";
-		foca.style.left = "100vw";
-		foca.style.opacity = "100%";
+
+		left.style.opacity = "100%";
+		left.style.visibility = "visible";
+		right.style.opacity = "100%";
+		right.style.visibility = "visible";
 	};
 
 
